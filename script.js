b9826b8bac96385ceb79844b7113c05464a0e1fa
Austin <ahalbritter@hotmail.com>
2021-07-30 16:54:48 -0400

    Final Touches

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 /**
  * Board
  */
-const boardSquares = [0, 1, 2, 3, 4, 5, 6, 8]
+const boardSquares = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
 /**
  * Winning Square Placements
  */
-const winningCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]] 
+const winningCombos = 
+[
+[0, 1, 2], 
+[3, 4, 5], 
+[6, 7, 8], 
+[0, 3, 6], 
+[1, 4, 7], 
+[2, 5, 8], 
+[0, 4, 8], 
+[2, 4, 6]
+] 
 
 /*
 0, 1, 2
@@ -19,6 +29,11 @@ const winningCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2
  */
 
 const functions = {
+
+    // Select Names for X & O Player
+    nameSelect: (xPlayer, oPlayer) => {
+        return `${xPlayer} is X and ${oPlayer} is O, ${xPlayer} take the first move`
+    },
     // Player X must go first
     firstPlayer: (player) => {
         if(player === 'x'){
@@ -29,18 +44,27 @@ const functions = {
     },
 
     // Removes the possibility to place X or O on square that is already used
+    // Returns Current Player Tile Placement
     moveTracker: (squareNum, lastPlayer, currentPlayer) => {
         const boardCopy = [...boardSquares];
         const indexOfSquareNum = boardCopy.indexOf(squareNum);
         if(boardCopy.includes(squareNum) && lastPlayer !== currentPlayer){
             boardCopy.splice(indexOfSquareNum, 1)
-            return true
+            return true && `${currentPlayer} has placed in square ${squareNum}`
         } else return false
+    },
+
+    // Resets the board once winner is announced
+    gameReset: (gameOver) => {
+        if(gameOver) {
+         let boardCopy = [...boardSquares];
+            return boardCopy;
+        } else return false;
     },
 
     // Checks if placed squares for player equal winning combination
     checkForWinner: (nums, currentPlayer) => {
-        let winner = "Game In Progress";
+        let winner;
         const sortedNums = nums.sort((a,b) => a - b)
         winningCombos.forEach((arr) => {
             if(arr[0] === sortedNums[0] && arr[1] === sortedNums[1] && arr[2] === sortedNums[2]){
@@ -49,11 +73,21 @@ const functions = {
         })
         if(winner === true) 
             {
-           return `${currentPlayer} is the winner`; 
-        } else return winner;
+           return `${currentPlayer} is the winner!`; 
+        } else return "Game In Progress";
 
+        },
+
+    // Visualization for tile placement in Console for caller
+    boardVisualization: (squareNum, currentPlayer) => {
+        let board = ["", "", "", "", "", "", "", "", ""];
+        for(let i=0; i<board.length; i++) {
+        board[squareNum] = `${currentPlayer}:${squareNum}` 
+        console.log(board);
+        return board;
         }
 
+    }
 };
 
 module.exports = functions;
--- script.test.js
+++ script.test.js
@@ -1,27 +1,40 @@
 const functions = require('./script');
 
+// Name Select [TRUTHY]
+test('Add Name for X and O Player', () => {
+    expect(functions.nameSelect("Austin", "Mike")).toBe("Austin is X and Mike is O, Austin take the first move")
+})
+
 // First Player Must be X [FALSY]
 test('Checks for First Move (must be X!) ', () => {
     expect(functions.firstPlayer('o')).toBe(false);
 })
 
 // Tracks Each Player Move & No Player Can Move Twice in a Row [TRUTHY]
-test('Tracks Each Player Move (correct placement)', () => {
-    expect(functions.moveTracker(1, "x", "o")).toBe(true);
-})
-
-// Tracks Each Player Move & No Player Can Move Twice in a Row [FALSY]
 // Removing Available Squares Once Valid Placement is True
-test('Tracks Each Player Move (player attempt to move twice in a row)', () => {
+test('Tracks Each Player Move (correct placement)', () => {
+    expect(functions.moveTracker(1, "x", "o")).toBe("o has placed in square 1");
+    expect(functions.moveTracker(5, "o", "x")).toBe("x has placed in square 5");
+// Tracks Each Player Move & No Player Can Move Twice in a Row [FALSY]
     expect(functions.moveTracker(6, "x", "x")).toBe(false);
+    expect(functions.moveTracker(3, "o", "o")).toBe(false);
 })
 
+test('Game over & Reset Board', () => {
+    expect(functions.gameReset('gameOver')).toStrictEqual([0, 1, 2, 3, 4, 5, 6, 7, 8]);
+    expect(functions.gameReset()).toBe(false);
+})
 // Checks Winning Player [TRUTHY]
-test('Checks for Winning Player', () => {
-    expect(functions.checkForWinner([0, 3, 6], 'x')).toBe('x is the winner')
+test('Checks for Winning Player or No Winner Yet', () => {
+    expect(functions.checkForWinner([0, 3, 6], 'x')).toBe('x is the winner!');
+// Checks Winning Player [FALSY]
+    expect(functions.checkForWinner([0, 3, 2], 'x')).toBe("Game In Progress");
 })
 
-// Checks Winning Player [FALSY]
-test('Checks for No Winner Yet', () => {
-    expect(functions.checkForWinner([0, 3, 2], 'x')).toBe("Game In Progress")
+//Board Visualization 
+test('Board Visualization', () => {
+    //Board Visualiation for API Caller
+    expect(functions.boardVisualization(3, "x")).toStrictEqual(["", "", "", "x:3", "", "", "", "", ""]);
+    expect(functions.boardVisualization(5, "o")).toStrictEqual(["", "", "", "", "", "o:5", "", "", ""]);
+
 })
